14fdf25f574488c83dab8771816748a36c0784af
Mateo Mansilla <mateobariloche28@gmail.com>
2022-05-25 00:36:01 -0300

    Arreglando

---
+++ App/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,40 @@
+import {useEffect, useState} from 'react'
+import { useParams } from 'react-router-dom'
+import ItemDetail from '../ItemDetail/ItemDetail'
+import {getFetch} from '../helpers/getFetch'
+
+export const ItemDetailContainer = () => {
+    const [product, setProduct] = useState({})
+    const [loader, setLoader] = useState(true)
+
+    
+    const {id} = useParams()
+    
+    useEffect(()=>{
+        getFetch(id)//fetch que llama a la api
+            .then((res) => {setProduct(res)})
+            .catch((err) => {console.log(err)})
+            .finally(() => setLoader(false))
+            
+    },[])
+
+    return(
+        <div>
+            {loader ?
+            <h2>Loading...</h2>
+            :    
+
+            <ItemDetail product = {product} />
+
+            }
+        </div>
+        )
+    }
+    
+
+            
+            
+            
+            
+
+
