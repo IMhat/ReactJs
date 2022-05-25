4d3992d8cfd5ed691a0a7970a4e7dd68dfaa8c3f
Mateo Mansilla <mateobariloche28@gmail.com>
2022-05-25 00:36:22 -0300

    Arreglando

---
+++ App/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+
+import {NavLink} from "react-router-dom";
+import CartWidget from "../CartWidget/CartWidget";
+import "./Navbar.css";
+import logo from '../../img/logo1.png'
+import { FiMenu } from "react-icons/fi";
+import { FiChevronLeft } from "react-icons/fi";
+
+
+function Navbar (){
+    return(
+        <>
+            <header className="header">
+                <div className="container">
+                    <div className="btn-menu">
+                        <label htmlFor="btn-menu" className="icon-menu"> <FiMenu/> </label>
+                    </div>
+                    <div className="logo">
+                        <img src={logo} alt="logo" width={50}/>
+                    </div>
+                        <nav className="menu">
+                        <NavLink to = '/'>Inicio</NavLink>
+                        <NavLink to = '/'>Nosotros</NavLink>
+                        <NavLink to = '/'>Contacto</NavLink>
+                        </nav>
+                </div>
+            </header>
+                            
+            <div className="capa"></div>
+            <input type="checkbox" id="btn-menu" />
+            <div className="container-menu">
+                <div className="cont-menu">
+                    <nav>
+                        <NavLink to = '/'>Inicio</NavLink>
+                        <NavLink to = '/category/TarjetasGraficas'>Tarjetas Graficas</NavLink>
+                        <NavLink to = '/category/Procesadores'>Procesadores</NavLink>
+                        <NavLink to = '/category/Gabinetes'>Gabinetes</NavLink>
+                        <CartWidget/>
+                    </nav>
+                    <label htmlFor="btn-menu" className="icon-equis"><FiChevronLeft/></label>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+export default Navbar
